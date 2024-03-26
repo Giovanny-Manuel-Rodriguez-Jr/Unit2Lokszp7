23b0613ae7ef3e0419d804810fb0933dbacf7293
Giovanny Rodriguez Jr <751764@student.dusd.net>
2024-03-26 14:51:43 -0700

    done with challenge

---
+++ Unit2Loksz/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeLimit = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimit = timeLimit - Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (timeLimit <= 0.0f)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timeLimit = 1.0f;
+            }
+            else if (timeLimit <= 1.0f)
+            {
+                return;
+            }
+            
+        }
+    }
+}
